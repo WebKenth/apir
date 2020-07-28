527cfb72a60cd547dbe18e0ad1aed4a67b14528f
Martin Knudsen <mkk@webkenth.dk>
2020-07-28 12:26:56 +0200

    10 horizon processes

--- config/horizon.php
+++ config/horizon.php
@@ -170,7 +170,7 @@ return [
                 'connection' => 'redis',
                 'queue' => ['default'],
                 'balance' => 'simple',
-                'processes' => 4,
+                'processes' => 10,
                 'tries' => 1,
             ],
         ],
