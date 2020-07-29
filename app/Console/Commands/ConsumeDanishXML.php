d41a1e427598beddbe01bd1f39c8a107b10616ae
Martin Knudsen <mkk@webkenth.dk>
2020-07-29 20:03:24 +0200

    use log

--- app/Console/Commands/ConsumeDanishXML.php
+++ app/Console/Commands/ConsumeDanishXML.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Jobs\ProcessDanishCarEntry;
+use Illuminate\Support\Facades\Log;
 
 class ConsumeDanishXML extends Command
 {
